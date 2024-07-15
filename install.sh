e14aeca1583aad7d2e4f13beaa9c30a188431abc
Brian Orndorff <brian.orndorff@springhealth.com>
2024-07-15 16:09:51 -0700

    change around

--- install.sh
+++ install.sh
@@ -3,7 +3,3 @@
 # Update package list and install zsh and neovim
 sudo apt update && sudo apt install -y neovim
 
-export DOTFILES=/workspaces/.codespaces/.persistedshare/dotfiles
-
-mkdir -p $HOMR/.config/nvim
-ln -sf $DOTFILES/.vimrc $HOME/.config/nvim/init.lua

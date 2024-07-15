7f885759a1e5d0b892e62181b8ae521db6f8a5ba
Brian Orndorff <brian.orndorff@springhealth.com>
2024-07-15 16:10:12 -0700

    sync

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -55,12 +55,12 @@ vim.opt.rtp:prepend(lazypath)
 -- Plugins
 require("lazy").setup({
   {
-    "rose-pine/neovim",
+    "rebelot/kanagawa.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme rose-pine]])
+      vim.cmd([[colorscheme kanagawa-dragon]])
     end,
   },
   {
@@ -77,38 +77,44 @@ require("lazy").setup({
       {'gd', "<cmd>Telescope lsp_definitions<cr>", desc = "lsp defs"},
       {'gr', "<cmd>Telescope lsp_references<cr>", desc = "lsp refs"},
     },
-    config = function()
+    config = function ()
       require('telescope').setup()
     end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    lazy = false,
-    config = function ()
+    config = function()
       local configs = require("nvim-treesitter.configs")
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "ruby", "javascript", "html" },
+
       configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "ruby" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },
         })
     end
   },
-  -- LSP packages, following docs from https://lsp-zero.netlify.app/v3.x/getting-started.html
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-  {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  {'L3MON4D3/LuaSnip'},
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup()
+    end
+  }
 })
-
--- LSP setup
-local lsp_zero = require('lsp-zero')
-lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-
--- Ruby LSP
-require('lspconfig').solargraph.setup({})
 

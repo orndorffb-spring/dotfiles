921e890b49bcaa42988f37838c07f4e9349af0ca
Brian Orndorff <126022674+orndorffb-spring@users.noreply.github.com>
2024-07-29 14:32:48 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,15 @@
 
 echo "Beginning setting up Dotfiles..."
 # Update package list and install zsh and neovim
-sudo apt update && sudo apt install -y zsh fzf tmux neovim
+sudo apt update && sudo apt install -y zsh fzf tmux
+
+# Install specific version of neovim
+# I'm using v0.9.5
+wget -O $HOME/nvim.appimage https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
+chmod 755 $HOME/nvim.appimage
+sudo $HOME/nvim.appimage --appimage-extract
+sudo mv ./squashfs-root $HOME/nvim
+sudo ln $HOME/nvim/usr/bin/nvim /usr/bin/nvim
 
 # Symlink various dotfiles to home directory
 export DOTFILES=/workspaces/.codespaces/.persistedshare/dotfiles

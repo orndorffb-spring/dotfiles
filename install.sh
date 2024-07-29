29df4709275818c791d799e62804ff40ae360568
Brian Orndorff <126022674+orndorffb-spring@users.noreply.github.com>
2024-07-29 16:45:53 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,27 +13,21 @@ sudo $HOME/nvim.appimage --appimage-extract
 sudo mv ./squashfs-root $HOME/nvim
 sudo ln $HOME/nvim/usr/bin/nvim /usr/bin/nvim
 
-
+# Install starship
+curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 # Symlink various dotfiles to home directory
 export DOTFILES=/workspaces/.codespaces/.persistedshare/dotfiles
 
-ln -sf $DOTFILES/.tmux.conf $HOME/.tmux.conf
-
-
-## Neovim configuration setup
 mkdir -p $HOME/.config/nvim
 ln -sf $DOTFILES/init.lua $HOME/.config/nvim/init.lua
-
+ln -sf $DOTFILES/.tmux.conf $HOME/.tmux.conf
 ln -sf $DOTFILES/starship.toml $HOME/.config/starship.toml
 ln -sf $DOTFILES/config.fish $HOME/.config/fish/config.fish
 
-echo "export VIMRUNTIME=$HOME/nvim/usr/share/nvim/runtime" >> ~/.zshrc
-
-# Install starship
-curl -sS https://starship.rs/install.sh | sh -s -- -y
+echo "export VIMRUNTIME=$HOME/nvim/usr/share/nvim/runtime" >> ~/.config/fish/config.fish
 
 # Set fish as the default shell
 sudo chsh "$(id -un)" --shell "/usr/bin/fish"
 
-echo "Done setting up Dotfiles!
+echo "Done setting up Dotfiles!"

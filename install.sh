ace5069a7c543fc83682e08dbbeb750ea3daa719
Brian Orndorff <126022674+orndorffb-spring@users.noreply.github.com>
2024-07-29 14:12:07 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # Update package list and install zsh and neovim
-sudo apt update && sudo apt install -y neovim
+sudo apt update && sudo apt install -y zsh fzf tmux neovim
+
+## Neovim configuration setup
+mkdir -p $HOME/.config/nvim
+ln -sf $DOTFILES/init.vim $HOME/.config/nvim/init.vim
 

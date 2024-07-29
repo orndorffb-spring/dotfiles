5d1caf1dbc8791226d1b90887d722f6dbf925cae
Brian Orndorff <126022674+orndorffb-spring@users.noreply.github.com>
2024-07-29 16:46:11 -0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,1 +1,19 @@
+alias nv="nvim"
+
+# Some tmux-related shell aliases
+
+# Attaches tmux to the last session; creates a new session if none exists.
+alias t='tmux attach || tmux new-session'
+
+# Attaches tmux to a session (example: ta portal)
+alias ta='tmux attach -t'
+
+# Creates a new session
+alias tn='tmux new-session'
+
+alias dc='docker-compose'
+
+# Lists all ongoing sessions
+alias tl='tmux list-sessions'
+
 starship init fish | source

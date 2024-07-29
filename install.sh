b9c67001ddf0447c5f5eb75c9bfcc4509c18eddf
Brian Orndorff <126022674+orndorffb-spring@users.noreply.github.com>
2024-07-29 14:19:34 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+
+echo "Beginning setting up Dotfiles..."
 # Update package list and install zsh and neovim
 sudo apt update && sudo apt install -y zsh fzf tmux neovim
 
+# Symlink various dotfiles to home directory
+export DOTFILES=/workspaces/.codespaces/.persistedshare/dotfiles
+ln -sf $DOTFILES/.tmux.conf $HOME/.tmux.conf
+
+
 ## Neovim configuration setup
 mkdir -p $HOME/.config/nvim
-ln -sf $DOTFILES/init.vim $HOME/.config/nvim/init.vim
+ln -sf $DOTFILES/init.lua $HOME/.config/nvim/init.lua
 
+echo "Done setting up Dotfiles!

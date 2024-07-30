17dd554bb8ec262718e8658f565ef738a283ec1c
Brian Orndorff <126022674+orndorffb-spring@users.noreply.github.com>
2024-07-30 15:28:54 -0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -15,4 +15,9 @@ alias tl='tmux list-sessions'
 
 export VIMRUNTIME=$HOME/nvim/usr/share/nvim/runtime
 
+function setup_fisher_and_nvm
+  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher  
+  fisher install jorgebucaran/nvm.fish
+end
+
 starship init fish | source

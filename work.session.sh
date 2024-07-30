99b885bdfcec320a5417264cc773ac985d3cbe33
Brian Orndorff <126022674+orndorffb-spring@users.noreply.github.com>
2024-07-30 12:04:46 -0700

    Update work.session.sh

--- work.session.sh
+++ work.session.sh
@@ -2,12 +2,18 @@ session_root "/workspaces/development-space"
 if initialize_session "work"; then
 
   new_window "rotom"
-  run_cmd "cd rotom"
-  run_cmd "clear"
+  run_cmd "cd rotom && clear"
+  split_v 50
+  run_cmd "cd rotom && clear"
+  select_pane 0
+  run_cmd "hx ."
 
-  new_window "ehr"
-  run_cmd "cd ehr-api"
-  run_cmd "clear"
+  new_window "ehr-api"
+  run_cmd "cd ehr-api && clear"
+  split_v 50
+  run_cmd "cd ehr-api && clear"
+  select_pane 0
+  run_cmd "hx ."
 
   select_window 1
 

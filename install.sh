28aad4f2c8f23a40347be99581b38362b0a4d784
Brian Orndorff <126022674+orndorffb-spring@users.noreply.github.com>
2024-07-30 10:58:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,21 @@
 
 
 echo "Beginning setting up Dotfiles..."
-sudo add-apt-repository ppa:maveonair/helix-editor
-sudo apt update && sudo apt install -y fzf tmux ripgrep fish helix
+sudo apt update && sudo apt install -y fzf tmux ripgrep fish
 
-# Install specific version of neovim
-# I'm using v0.9.5
+# Install nivm
 wget -O $HOME/nvim.appimage https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
 chmod 755 $HOME/nvim.appimage
 sudo $HOME/nvim.appimage --appimage-extract
 sudo mv ./squashfs-root $HOME/nvim
 sudo ln $HOME/nvim/usr/bin/nvim /usr/bin/nvim
+
+# Install hx
+wget -O $HOME/helix.appimage https://github.com/helix-editor/helix/releases/download/24.07/helix-24.07-x86_64.AppImage
+chmod 755 $HOME/helix.appimage
+sudo $HOME/helix.appimage --appimage-extract
+sudo mv ./squashfs-root $HOME/helix
+sudo ln $HOME/hx/usr/bin/hx /usr/bin/hx
 
 # Install starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y

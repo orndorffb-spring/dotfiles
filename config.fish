57bdff325d271211ad9451b3175d1514b20694c3
Brian Orndorff <126022674+orndorffb-spring@users.noreply.github.com>
2024-08-05 13:52:21 -0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -21,9 +21,9 @@ function lint-rb
 end
 
 function setup_fish_stuff
-  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher  
-  fisher install jorgebucaran/nvm.fish
-  curl -L --create-dirs -o ~/.config/fish/functions/rvm.fish https://raw.github.com/lunks/fish-nuggets/master/functions/rvm.fish
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+    fisher install jorgebucaran/nvm.fish
+    curl -L --create-dirs -o ~/.config/fish/functions/rvm.fish https://raw.github.com/lunks/fish-nuggets/master/functions/rvm.fish
 end
 
 function get_access_token
@@ -35,9 +35,21 @@ function get_access_token
     curl 'diglet:8080/oauth/access_token' \
         -H 'accept: application/json, text/plain, */*' \
         -H 'content-type: application/json' \
-        --data-raw '{"grant_type":"password","client_id":"bb96279d-65bb-4c90-91ed-e6c7e9ce4a70","client_secret":"'$EHR_TEST_DIGLET_SHAYMIN_SECRET'","scope":"group:admin group:medical_biller group:super_admin group:compass:care_provider group:compass:associate_provider group:compass:med_manager group:compass:coach group:compass:care_navigator group:compass:care_support_coordinator group:compass:admin group:compass:clinical_manager group:compass:clinical_support group:compass:super_admin group:compass:billing_specialist group:compass:care_consultant group:compass:care_advocate group:compass:group_practice_admin group:engineer","username":"'$argv[1]'@springhealth.com","password":"foobarbazz"}' | jq .access_token
+        --data-raw '{"grant_type":"password","client_id":"bb96279d-65bb-4c90-91ed-e6c7e9ce4a70","client_secret":"'$EHR_TEST_DIGLET_SHAYMIN_SECRET'","scope":"group:admin group:medical_biller group:super_admin group:compass:care_provider group:compass:associate_provider group:compass:med_manager group:compass:coach group:compass:care_navigator group:compass:care_support_coordinator group:compass:admin group:compass:clinical_manager group:compass:clinical_support group:compass:super_admin group:compass:billing_specialist group:compass:care_consultant group:compass:care_advocate group:compass:group_practice_admin group:engineer","username":"'$argv[1]'@springhealth.com","password":"foobarbazz"}' | jq -r .access_token
 end
 
+function set_at
+    set -gx access_token (get_access_token $argv[1])
+end
 
+function rotom_get
+    if test (count $argv) -lt 1
+        echo "Route needed" >&2
+        return 1
+    end
+
+    curl --location 'http://localhost:4400/api/compass/'$argv[1]'' \
+        --header 'Authorization: Bearer '$access_token'' | jq .
+end
 
 starship init fish | source
